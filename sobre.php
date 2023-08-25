72044947b9c0cab9f903dcd30444d9c7c48a1e1a
FILIPE FOGACA LOPES <210323@lti.fatecso.edu.br>
2023-08-25 14:54:47 -0300

    modificaçao de background do sobre

--- sobre.php
+++ sobre.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
 
-<?php
-    require_once "config.php";
-    include(HEADER_TEMPLATE);
-?>
+    <?php
+        require_once "config.php";
+        include(HEADER_TEMPLATE);
+    ?>
 
 <html>
     <head>
+        <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="inc/style.css"/>
 
+        <title>Document</title>
     </head>
     <body>
-            
+        <div class="bgsolda">
+            <h2>Sobre</h2>
+        </div>
+
+        <script src="inc/script.js"></script>
     </body>
 </html>
