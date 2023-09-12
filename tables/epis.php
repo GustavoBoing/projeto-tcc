e9c2976c88945615082bacea5ff699fb47c58d26
Gustavo <gustavo.boing.lima2006@gmail.com>
2023-09-12 11:00:06 -0300

    Gustavo Boing: modificações na tela das tabelas

--- tables/epis.php
+++ tables/epis.php
@@ -10,17 +10,18 @@
         <link rel="stylesheet" href="../inc/style.css"/>
     </head>
     <body>
-        <br>
-        <h2> ESTOQUE - EPI´s </h2>
-        <hr>
-        <br> 
-
-            <div class="pill-nav">
-                 <a href="#adicionar">Adicionar</a>
-                 <a href="#editar">Editar</a>
+        <header>
+            <h2> ESTOQUE - EPI´s </h2>
+            <hr>
+            <div id="botoes">
+                <div class="pill-nav" id="adicionar">
+                    <a href="#contact">Adicionar</a>
+                </div>
+                <div class="pill-nav" id="editar">
+                    <a href="#about">Editar</a>
+                </div>
             </div>
-
-        
+        </header>
         <table class="content-table">
             <thead>
                 <tr>
--- tables/insumos.php
+++ tables/insumos.php
@@ -10,17 +10,19 @@
         <link rel="stylesheet" href="../inc/style.css"/>
     </head>
     <body>
-        <br>
-        <h2> ESTOQUE - INSUMOS </h2>
-        <hr>
-        <br> 
-        
-            <div class="pill-nav">
-                <a href="#contact">Adicionar</a>
-                <a href="#about">Editar</a>
+        <header>
+            <h2> ESTOQUE - INSUMOS </h2>
+            <hr>
+            <div id="botoes">
+                <div class="pill-nav" id="adicionar">
+                    <a href="#contact">Adicionar</a>
+                </div>
+                <div class="pill-nav" id="editar">
+                    <a href="#about">Editar</a>
+                </div>
             </div>
-        </div>
-        
+        </header>
+
         <table class="content-table">
             <thead>
                 <tr>
