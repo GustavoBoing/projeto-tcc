2ba937cc2a116cda9adf2ac79629434af894102a
Filipe <filipefogaca4@gmail.com>
2023-09-12 21:55:45 -0300

    alteraçao no tecto e no sobre

--- sobre.php
+++ sobre.php
@@ -1,37 +1,77 @@
 <!DOCTYPE html>
 
-    <?php
-        require_once "config.php";
-        include(HEADER_TEMPLATE);
-    ?>
+<?php
+require_once "config.php";
+include(HEADER_TEMPLATE);
+?>
 
 <html>
-    <head>
-        <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" href="./inc/style.css"/>
-        <title>Document</title>
-    </head>
-    
-    <body>
-        <div class="bgsolda">
-            <h2>Sobre</h2>
-        </div>
-        <main>
-            <div class="textoSobre">
-                <p>Temos o prazer de apresentar nossa Plataforma de Controle de Almoxarifado na versão 1.0.0, uma solução desenvolvida com dedicação e expertise para otimizar a gestão de materiais na RV Soluções Industriais. Nosso sistema foi projetado para atender às demandas específicas da empresa, proporcionando uma abordagem eficaz e eficiente para administrar tanto insumos quanto Equipamentos de Proteção Individual (EPIs).
-                <div class="cTitulo"><p>Características Principais:</p></div>
-                <div class="topicos">
-                    <p><b>Segurança Aperfeiçoada:</b> A Plataforma de Controle de Almoxarifado prioriza a segurança de informações sensíveis. Através da tela de login, garantimos que somente usuários autorizados tenham acesso ao sistema, protegendo seus dados e mantendo a confidencialidade.</p>
-                    <p><strong>Visão Global no Início:</strong> Ao adentrar o sistema, nossa tela de início oferece uma visão abrangente da situação do almoxarifado. Apresentamos gráficos intuitivos e cards informativos que destacam materiais em grande quantidade, aqueles em escassez e os itens mais utilizados. Essa visualização estratégica auxilia na tomada de decisões assertivas.</p>
-                    <p><strong>Gerenciamento Preciso de Insumos e EPIs:</strong> Nossa plataforma dispõe de telas específicas para insumos e EPIs. Através de tabelas interativas, os usuários têm a possibilidade de ajustar as quantidades de materiais, seja para retirada ou adição. Isso assegura a manutenção do estoque conforme as necessidades operacionais.</p>
-                    <p><strong>Transparência no Histórico:</strong> A funcionalidade de histórico de transações permite rastrear as ações mais recentes realizadas no sistema. Cada movimentação, seja relacionada a insumos ou EPIs, é registrada, garantindo transparência e fornecendo insights valiosos para análises futuras.</p>
-                    <p><strong>Configurações Personalizadas:</strong> A tela de configurações proporciona uma maneira flexível de personalizar a experiência do usuário. Mesmo que os detalhes específicos desta seção estejam sendo finalizados, nosso sistema já oferece um espaço para você definir parâmetros conforme suas necessidades.</p>
-                    <p>A Plataforma de Controle de Almoxarifado é uma ferramenta que alavanca a eficiência, a organização e a tomada de decisões informadas dentro do nosso ambiente industrial. Nosso compromisso é continuar desenvolvendo recursos e funcionalidades que atendam às exigências dinâmicas de nossa empresa, garantindo uma gestão de materiais precisa e estratégica.</p>
-                </div>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./inc/style.css" />
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="bgsolda">
+        <h2>Sobre</h2>
+    </div>
+    <main>
+        <div class="textoSobre">
+            <p>Temos o prazer de apresentar nossa Plataforma de Controle de Almoxarifado na versão 1.0.0, uma solução
+                desenvolvida com dedicação e expertise para otimizar a gestão de materiais na RV Soluções Industriais.
+                Nosso sistema foi projetado para atender às demandas específicas da empresa, proporcionando uma
+                abordagem eficaz e eficiente para administrar tanto insumos quanto Equipamentos de Proteção Individual
+                (EPIs).
+            <div class="cTitulo">
+                <p>Características Principais:</p>
             </div>
-        </main>
-        <script src="js/script.js"></script>
-    </body>
+            <div class="topicos">
+                <p><b>Segurança Aperfeiçoada:</b> A Plataforma de Controle de Almoxarifado prioriza a segurança de
+                    informações sensíveis. Através da tela de login, garantimos que somente usuários autorizados tenham
+                    acesso ao sistema, protegendo seus dados e mantendo a confidencialidade.</p>
+                <p><strong>Visão Global no Início:</strong> Ao adentrar o sistema, nossa tela de início oferece uma
+                    visão abrangente da situação do almoxarifado. Apresentamos gráficos intuitivos e cards informativos
+                    que destacam materiais em grande quantidade, aqueles em escassez e os itens mais utilizados. Essa
+                    visualização estratégica auxilia na tomada de decisões assertivas.</p>
+                <p><strong>Gerenciamento Preciso de Insumos e EPIs:</strong> Nossa plataforma dispõe de telas
+                    específicas para insumos e EPIs. Através de tabelas interativas, os usuários têm a possibilidade de
+                    ajustar as quantidades de materiais, seja para retirada ou adição. Isso assegura a manutenção do
+                    estoque conforme as necessidades operacionais.</p>
+                <p><strong>Transparência no Histórico:</strong> A funcionalidade de histórico de transações permite
+                    rastrear as ações mais recentes realizadas no sistema. Cada movimentação, seja relacionada a insumos
+                    ou EPIs, é registrada, garantindo transparência e fornecendo insights valiosos para análises
+                    futuras.</p>
+                <p><strong>Configurações Personalizadas:</strong> A tela de configurações proporciona uma maneira
+                    flexível de personalizar a experiência do usuário. Mesmo que os detalhes específicos desta seção
+                    estejam sendo finalizados, nosso sistema já oferece um espaço para você definir parâmetros conforme
+                    suas necessidades.</p>
+                <p>A Plataforma de Controle de Almoxarifado é uma ferramenta que alavanca a eficiência, a organização e
+                    a tomada de decisões informadas dentro do nosso ambiente industrial. Nosso compromisso é continuar
+                    desenvolvendo recursos e funcionalidades que atendam às exigências dinâmicas de nossa empresa,
+                    garantindo uma gestão de materiais precisa e estratégica.</p>
+            </div>
+        </div>
+
+        <div class="Divisor" data-aos-anchor-placement="top-bottom">
+            <h2>Protocolos Estéticos</h2>
+        </div>
+        
+            <div class="linha-title">
+                <img src="https://esteticavanessagimenez.com.br/template/imagens/icone.png" alt="Icone">
+            </div>
+        
+        <div class="box">
+            <span>01</span>
+            <h4>Tratamentos Corporais</h4>
+            <p>Temos os melhores tratamentos estéticos corporais de Sorocaba! Conheça e se encante com os resultados.
+            </p>
+        </div>
+    </main>
+    <script src="js/script.js"></script>
+</body>
+
 </html>
