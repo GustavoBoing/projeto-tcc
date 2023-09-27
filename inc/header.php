fe1a30f75366c32d1db834b255a5c891a04f4808
Gustavo <gustavo.boing.lima2006@gmail.com>
2023-09-27 12:00:46 -0300

    melhora no header

--- inc/header.php
+++ inc/header.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="./style.css"/>
-    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="<?php echo BASEURL; ?>inc/style.css"/>
+    <script src="<?php echo BASEURL; ?>js/script.js"></script>
   </head>
 
   <header>
