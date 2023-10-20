40b7f166d502b5016ac5c4fe094e5dca24021235
BEATRIZ CACAO ALAMINO <210339@lti.fatecso.edu.br>
2023-10-20 14:55:54 -0300

    mudança nas tabelas

--- tables/insumos.php
+++ tables/insumos.php
@@ -16,14 +16,14 @@
         <header>
             <h2> ESTOQUE - INSUMOS </h2>
             <hr>
-            <div id="botoes">
+            <!-- <div id="botoes">
                 <div class="pill-nav" id="adicionar">
                     <a href="#contact">Adicionar</a>
                 </div>
                 <div class="pill-nav" id="editar">
                     <a href="#about">Editar</a>
                 </div>
-            </div>
+            </div> -->
         </header>
         <main>
             <table class="content-table">
@@ -33,23 +33,33 @@
                         <th>Insumo</th>
                         <th>Tipo</th>
                         <th>Quantidade</th>
+                        <th>Alterações</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php if ($produtos) : ?>
                         <?php foreach($produtos as $produto) :?>
                             <tr>
-                                <td><?php echo $produto['id_produto']?></td>
-                                <td><?php echo $produto['Descricao']?></td>
-                                <td><?php echo $produto['Tipo']?></td>
-                                <td><?php if($produto['Quantidade'] < 10 ){
-                                    echo '<span class="vermelho">' . $produto['Quantidade'] . '</span>';
-                                } elseif ($produto['Quantidade'] <= 40){
-                                    echo '<span class="amarelo">' . $produto['Quantidade'] . '</span>';
-                                } else {
-                                    echo '<span class="verde">' . $produto['Quantidade'] . '</span>';
-                                }
-                                ?></td>
+                                <?php 
+                                if ($produto['Tipo'] == '2') 
+                                {
+                                    echo '<td>'.$produto['id_produto'].'</td>';
+                                    echo '<td>'.$produto['Descricao'].'</td>';
+                                    echo '<td>'.$produto['Tipo'].'</td>';
+                                    if($produto['Quantidade'] < 10 ){
+                                        echo '<td>' . '<span class="vermelho">' . $produto['Quantidade'] . '</span>' . '</td>';
+                                    } else if ($produto['Quantidade'] <= 40){
+                                         echo '<td>' . '<span class="amarelo">' . $produto['Quantidade'] . '</span>' . '</td>' ;
+                                    } else {
+                                         echo '<td>' . '<span class="verde">' . $produto['Quantidade'] . '</span>' . '</td>';
+                                    }
+                                ?>
+                                <td class="changes">
+                                    <a href="edit.php?id=<?php echo $customer['id']; ?>" class="btn btn-sm btn-secondary"><i class='bx bx-plus-circle'></i> Adicionar</a>
+                                    <a href="edit.php?id=<?php echo $customer['id']; ?>" class="btn btn-sm btn-secondary"><i class='bx bx-minus-circle'></i> Retirar</a>
+			                        <a href="edit.php?id=<?php echo $customer['id']; ?>" class="btn btn-sm btn-secondary"><i class="fa-solid fa-user-pen"></i> Editar</a>
+                                </td>
+                                <?php } ?>
                             </tr>
                         <?php endforeach ; ?>    
                     <?php endif ; ?>
