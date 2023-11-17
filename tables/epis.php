cf0702db748d07fdbd0ee680cf7cafc0235712b9
FILIPE FOGACA LOPES <210323@lti.fatecso.edu.br>
2023-11-17 15:14:06 -0300

    alteraçao Telas e FGC

--- tables/epis.php
+++ tables/epis.php
@@ -1,10 +1,9 @@
-
+<!DOCTYPE html>
     <?php
         require_once('function.php');
         indexEPI();
         include(HEADER_TEMPLATE);
     ?>
-<!DOCTYPE html>
 <html>
     <head>
         <link rel="stylesheet" href="styleTbl.css"/>
@@ -12,21 +11,16 @@
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="<?php echo BASEURL; ?>inc/style.css"/>
         <link rel="stylesheet" href="<?php echo BASEURL; ?>inc/styleDark.css">
-        <script src="<?php echo BASEURL; ?>inc/script.js"defer></script>
     </head>
     <body>
-        <header>
+        <div class="tittle">
+            <h2 class="titulos"><i class='bx bx-hard-hat'></i>&nbsp EPI's </h2>
+            <p id="subtitulo" style="font-size:small; margin:0 0 0 70px">Visão geral dos itens de EPI</p>
+        </div>
+        <!-- <header>
             <h2>&nbsp<i class='bx bx-hard-hat' id="icons"></i>&nbsp &nbsp ESTOQUE - EPI´s </h2>
             <hr>
-            <!-- <div id="botoes">
-                <div class="pill-nav" id="adicionar">
-                    <a href="#contact">Adicionar</a>
-                </div>
-                <div class="pill-nav" id="editar">
-                    <a href="#about">Editar</a>
-                </div>
-            </div> -->
-        </header>
+        </header> -->
         <main>
             <table class="content-table">
                 <thead>
@@ -72,28 +66,111 @@
                         </tr>
                     <?php endif ; ?>
                 </tbody>
-                <!-- <tr>
-                    <td>001</td>
-                    <td>Óculos de segurança</td>
-                    <td>EPI</td>
-                    <td>
-                        <?php
-                            // for ($j = 0; $j < 1; $j++) {
-                            //     $valor = rand(1,100);
-                            // }
-                            // if ($valor < 10) {
-                            //     echo '<span class="vermelho">' . $valor . '</span>';
-                            // } elseif ($valor >= 11 && $valor <= 40) {
-                            //     echo '<span class="amarelo">' . $valor . '</span>';
-                            // } else {
-                            //     echo '<span class="verde">' . $valor . '</span>';
-                            // }
-                        ?>-->
             </table>
         </main>
 
+        <div class="w3-container">
+            <div id="id01" class="w3-modal">
+                <div class="w3-modal-content">
+                <header class="w3-container w3-black w3-center"> 
+                    <span onclick="document.getElementById('id01').style.display='none'" 
+                    class="w3-button w3-display-topright">&times;</span>
+                    <h2>Adicionar</h2>
+                </header>
+                <div class="w3-container">
+                    <form method="POST" id="form-edit-usuario">
+                        <input type="hidden" name="id" id="editid">
+                        <div class="row mb-3 w3-padding-16">
+                            <label for="quantidade" class="col-sm-2 col-form-label">Quantidade</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="quantidade" class="form-control" id="editquantidade" placeholder="Quantidade">
+                            </div>
+                        </div>
 
+                        <button type="submit" class="btn btn-outline-warning btn-sm" value="Salvar">Salvar</button>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="w3-container">
+            <div id="id02" class="w3-modal">
+                <div class="w3-modal-content">
+                <header class="w3-container w3-teal"> 
+                    <span onclick="document.getElementById('id02').style.display='none'" 
+                    class="w3-button w3-display-topright">&times;</span>
+                    <h2>Retirar</h2>
+                </header>
+                <div class="w3-container">
+                <form method="POST" id="form-edit-usuario">
+                        <input type="hidden" name="id" id="editid">
+
+                        <div class="row mb-3">
+                            <label for="quantidade" class="col-sm-2 col-form-label">Quantidade</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="quantidade" class="form-control" id="editquantidade" placeholder="Quantidade">
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-outline-warning btn-sm" value="Salvar">Salvar</button>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="w3-container">
+            <div id="id03" class="w3-modal">
+                <div class="w3-modal-content">
+                <header class="w3-container w3-teal"> 
+                    <span onclick="document.getElementById('id03').style.display='none'" 
+                    class="w3-button w3-display-topright">&times;</span>
+                    <h2>Editar</h2>
+                </header>
+                <div class="w3-container">
+                    <form method="POST" id="form-edit-usuario">
+                        <input type="hidden" name="id" id="editid">
+
+                        <div class="row mb-3">
+                            <label for="quantidade" class="col-sm-2 col-form-label">Quantidade</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="quantidade" class="form-control" id="editquantidade" placeholder="Quantidade">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="valor" class="col-sm-2 col-form-label">Valor</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="valor" class="form-control" id="editvalor" placeholder="Valor">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="modelo" class="col-sm-2 col-form-label">Modelo</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="modelo" class="form-control" id="editmodelo" placeholder="Modelo">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="descricao" class="col-sm-2 col-form-label">Descrição</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="descricao" class="form-control" id="editdescricao" placeholder="Descrição">
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-outline-warning btn-sm" value="Salvar">Salvar</button>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
+            <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+            <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+            <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
             <script src="js/custom.js"></script>
-
+            <script src="<?php echo BASEURL; ?>inc/script.js"defer></script>
     </body>
 <html>
--- tables/insumos.php
+++ tables/insumos.php
@@ -1,9 +1,9 @@
-<?php
-    require_once('function.php');
-    indexINS();
-    include(HEADER_TEMPLATE);
-?>
 <!DOCTYPE html>
+    <?php
+        require_once('function.php');
+        indexINS();
+        include(HEADER_TEMPLATE);
+    ?>
 <html>
     <head>
         <link rel="stylesheet" href="styleTbl.css"/>
@@ -14,9 +14,12 @@
         <script src="<?php echo BASEURL; ?>inc/script.js"defer></script>
     </head>
     <body>
-        <header>
-            <h2>&nbsp <i class='bx bx-grid-alt' id="icons"></i>&nbsp &nbsp ESTOQUE - INSUMOS </h2>
-            <hr>
+        <div class="tittle">
+            <h2 class="titulos" style="color:#F06E2D; text-shadow: 1px 2px 5px black; letter-spacing:1px;"><i class='bx bx-grid-alt'></i>&nbsp Insumos </h2>
+            <p id="subtitulo" style="font-size:small; margin:0 0 0 70px">Visão geral dos itens de Insumo</p>
+        </div>
+            <!-- <h2>&nbsp <i class='bx bx-grid-alt' id="icons"></i>&nbsp &nbsp ESTOQUE - INSUMOS </h2>
+            <hr> -->
             <!-- <div id="botoes">
                 <div class="pill-nav" id="adicionar">
                     <a href="#contact">Adicionar</a>
@@ -25,7 +28,6 @@
                     <a href="#about">Editar</a>
                 </div>
             </div> -->
-        </header>
         <main>
             <table class="content-table">
                 <thead>
@@ -61,7 +63,7 @@
                                 <a href="edit.php?id=<?php echo $produto['id_produto']; ?>" class="btn btn-sm btn-secondary"><i class='bx bx-plus-circle'></i> Adicionar</a>
                                 <a href="edit.php?id=<?php echo $produto['id_produto']; ?>" class="btn btn-sm btn-secondary"><i class='bx bx-minus-circle'></i> Renovar</a>
                                 <a href="edit.php?id=<?php echo $produto['id_produto']; ?>" class="btn btn-sm btn-secondary"><i class="fa fa-edit"></i> Editar</a>
-                                
+                                </td>
                                 <?php } ?>
                             </tr>
                         <?php endforeach ; ?>    
@@ -69,6 +71,109 @@
                 </tbody>
             </table>
         </main>
-        <script src="js/custom.js"></script>
+
+    <div class= "modaltbl">                  
+        <div class="w3-container ">
+            <div id="id01" class="w3-modal">
+                <div class="w3-modal-content w3-round">
+
+                <header class="w3-container w3-black w3-center"> 
+                    <span onclick="document.getElementById('id01').style.display='none'" 
+                    class="w3-button w3-display-topright">&times;</span>
+                    <h2>Adicionar</h2>
+                </header>
+
+                <div class="w3-container">
+                    <form method="POST" id="form-edit-usuario">
+                        <input type="hidden" name="id" id="editid">
+                        <div class="row mb-3 w3-padding-16">
+                            <label for="quantidade" class="col-sm-2 col-form-label">Quantidade</label>
+                            <div class="col-sm-10">
+                                <input type="number" name="quantidade" class="form-control" id="editquantidade" placeholder="Digite a quantidade que deseja inserir">
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-outline-warning btn-sm" value="Salvar">Salvar</button>
+                        <hr>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div> 
+
+        <div class="w3-container">
+            <div id="id02" class="w3-modal">
+                <div class="w3-modal-content w3-round">
+                <header class="w3-container w3-black w3-center"> 
+                    <span onclick="document.getElementById('id02').style.display='none'" 
+                    class="w3-button w3-display-topright">&times;</span>
+                    <h2>Retirar</h2>
+                </header>
+                <div class="w3-container">
+                <form method="POST" id="form-edit-usuario">
+                        <input type="hidden" name="id" id="editid">
+
+                        <div class="row mb-3 w3-padding-16">
+                            <label for="quantidade" class="col-sm-2 col-form-label">Quantidade</label>
+                            <div class="col-sm-10">
+                                <input type="number" name="quantidade" class="form-control" min="0" id="editquantidade" placeholder="Quantidade">
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-outline-warning btn-sm" value="Salvar">Salvar</button>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="w3-container">
+            <div id="id03" class="w3-modal">
+                <div class="w3-modal-content w3-round">
+                <header class="w3-container w3-black w3-center"> 
+                    <span onclick="document.getElementById('id03').style.display='none'" 
+                    class="w3-button w3-display-topright">&times;</span>
+                    <h2>Editar</h2>
+                </header>
+                <div class="w3-container">
+                    <form method="POST" id="form-edit-usuario">
+                        <input type="hidden" name="id" id="editid">
+
+                        <div class="row mb-3 w3-padding-16">
+                            <label for="quantidade" class="col-sm-2 col-form-label">Quantidade</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="quantidade" class="form-control" id="editquantidade" placeholder="Quantidade">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3 w3-padding-16">
+                            <label for="valor" class="col-sm-2 col-form-label">Valor</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="valor" class="form-control" id="editvalor" placeholder="Valor">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3 w3-padding-16">
+                            <label for="modelo" class="col-sm-2 col-form-label">Modelo</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="modelo" class="form-control" id="editmodelo" placeholder="Modelo">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3 w3-padding-16">
+                            <label for="descricao" class="col-sm-2 col-form-label">Descrição</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="descricao" class="form-control" id="editdescricao" placeholder="Descrição">
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-outline-warning btn-sm" value="Salvar">Salvar</button>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
     </body>
+    <script src="<?php echo BASEURL ?>js/tabelas.js"></script>
 </html>
