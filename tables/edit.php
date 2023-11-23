2dd677e8ca715bcf2e5c2eb141eb4134a72602ae
Filipe <filipefogaca4@gmail.com>
2023-11-22 22:33:23 -0300

    css completo editar

--- tables/edit.php
+++ tables/edit.php
@@ -2,6 +2,9 @@
 session_start();
 
 include_once("conexao.php");
+require_once('function.php');
+indexEPI();
+include(HEADER_TEMPLATE);
 
 $id_produto = filter_input(INPUT_GET, 'id_produto', FILTER_SANITIZE_NUMBER_INT);
 $result_produto = "SELECT * FROM produto WHERE id_produto = '" . $_GET['id'] . "'";
@@ -11,21 +14,23 @@ $row_produto = mysqli_fetch_assoc($resultado_produto);
 <!DOCTYPE html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="style.css"><link rel="stylesheet" href="<?php echo BASEURL; ?>inc/style.css"/>
+        <link rel="stylesheet" href="<?php echo BASEURL; ?>inc/styleDark.css">
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-
-<body>
-    <div class="TituloEdit">
-        <h2 class="titulos"><i class='bx bx-hard-hat'></i>&nbsp Editar Dados </h2>
         <?php
         if (isset($_SESSION['msg'])) {
             echo $_SESSION['msg'];
             unset($_SESSION['msg']);
         }
         ?>
+<body>
+    <div class="TituloEdit">
+        <h2 class="titulos"><i class='bx bx-hard-hat'></i>&nbsp Editar Dados </h2>
+        </div>
         <form method="POST" action="proc_edit_prod.php">
             <input type="hidden" name="id" value="<?php echo $row_produto['id_produto']; ?>">
 
@@ -39,14 +44,14 @@ $row_produto = mysqli_fetch_assoc($resultado_produto);
             <div class="Qtd">
                 <label for="Qtd">
                     Quantidade:
-                    <input type="text" name="quantidade" placeholder="Digite a quantidade" value="<?php echo $row_produto['Quantidade']; ?>"><br><br>
+                    <input type="number" name="quantidade" placeholder="Digite a quantidade" value="<?php echo $row_produto['Quantidade']; ?>"><br><br>
                 </label>
             </div>
 
             <div class="Valor">
                 <label for="Valor">
                     Valor:
-                    <input type="text" name="valor" placeholder="Digite o valor" value="<?php echo $row_produto['Valor']; ?>"><br><br>
+                    <input type="number" step="0.01" name="Valor" min="0.01" placeholder="Digite o valor" value="<?php echo $row_produto['Valor']; ?>"><br><br>
                 </label>
             </div>
 
@@ -57,10 +62,20 @@ $row_produto = mysqli_fetch_assoc($resultado_produto);
                 </label>
             </div>
 
-            <button type="submit" class="btn btn-primary">Salvar</button>
-            <button type="submit" class="btn btn-primary">Cancelar</button>
         </form>
-    </div>
+
+        <div class= "btnfuncoes">  
+            <div class="btnSalvar">
+                <button type="submit" class="btn btn-primary">Salvar</button>
+            </div>
+
+            <div class="btnCancela">
+                <button type="submit" class="btn btn-primary">Cancelar</button>
+            </div>                
+        </div>
+
+
+        
 </body>
 
 </html>
